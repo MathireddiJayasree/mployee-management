19e1a1902a9bbc795c22ce68ed12479f72df8f6d
MathireddiJayasree <mathireddi.jayasree@capgemini.com>
2024-02-05 23:20:08 +0530

    initial commit

---
+++ Employee-management/src/test/java/java/guides/Employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package java.guides.Employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
